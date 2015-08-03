b1a42c36e9e5d2d6b6661b7d2d64602748454a2a
networkingyuppy <networkingyuppy@gmail.com>
2015-08-03 09:54:14 -0400

    signin

--- barako/tests/account/signIn.php
+++ barako/tests/account/signIn.php
@@ -1,54 +1,48 @@
 <?php
-class Example extends PHPUnit_Extensions_SeleniumTestCase
+
+class SignIn extends PHPUnit_Extensions_Selenium2TestCase
 {
-  protected function setUp()
-  {
-    $this->setBrowser("*chrome");
-    $this->setBrowserUrl("https://barako.liveauctioneers.com/");
-  }
-
-  public function testMyTestCase()
-  {
-    // Sign-In Login Process
-    $this->open("/");
-    // Login Button
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setHost('127.0.0.1');
+        $this->setPort(4444);
+        $this->setBrowserUrl('https://barako.liveauctioneers.com/');
+    }
+    
+    /** 
+     * Method testSignIn 
+     * @test 
+     */ 
+    public function testSignIn()
+    {
+        // Sign-In Login Process
+        $this->url("/");
+        // Login Button
+        // ERROR: Caught exception [TypeError: WDAPI.Utils.isElementPresent is not a function]
+        $this->byCssSelector("div.header-content > button:contains(Login)")->click();
+        // Sign Up Modal Window
+        // ERROR: Caught exception [TypeError: WDAPI.Utils.isElementPresent is not a function]
         try {
-            if ($this->isElementPresent("css=a.header-logo")) break;
-        } catch (Exception $e) {}
-        sleep(1);
-    }
-
-    $this->click("css=div.header-content > button:contains(Login)");
-    // Sign Up Modal Window
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
+            $result = $this->byCssSelector("p.subtitle")->text();
+        $this->assertEquals("Sign in to LiveAuctioneers", $result);
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->__toString());
+        }
+        // Login Credentials
+        // ERROR: Caught exception [TypeError: statement is not a function]
+        // ERROR: Caught exception [TypeError: statement is not a function]
+        // ERROR: Caught exception [TypeError: statement is not a function]
+        // Verify Logged in status
         try {
-            if ($this->isElementPresent("css=div.modal-inner")) break;
-        } catch (Exception $e) {}
-        sleep(1);
+            $this->assertNotEquals("", $this->byCssSelector("div.header-content > button:contain(Login)")->text());
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->__toString());
+        }
+        // ERROR: Caught exception [TypeError: WDAPI.Utils.isElementPresent is not a function]
     }
 
-    $this->verifyText("css=p.subtitle", "Sign in to LiveAuctioneers");
-    // Login Credentials
-    $this->type("name=username", "ben.fong@liveauctioneers.com");
-    $this->type("name=password", "123456");
-    $this->click("css=button:contains(Submit)");
-    // Verify Logged in status
-    try {
-        $this->assertNotEquals("", $this->getText("css=div.header-content > button:contain(Login)"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
-    }
-    for ($second = 0; ; $second++) {
-        if ($second >= 60) $this->fail("timeout");
-        try {
-            if ($this->isElementPresent("css=button > span.icon-person")) break;
-        } catch (Exception $e) {}
-        sleep(1);
-    }
-
-  }
 }
-?>
